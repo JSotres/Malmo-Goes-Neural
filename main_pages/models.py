18e73c5cb6a0743d7c97411e14b84d2b79f01633
Javier Sotres <jsotres@gmail.com>
2020-04-26 00:59:32 +0200

    short description 2 added to Project model

--- main_pages/models.py
+++ main_pages/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     name = models.CharField(max_length=200, default='project short name')
     longName = models.CharField(max_length=200, default='project long name')
     short_description = models.TextField(blank=True, default='short description')
+    short_description2 = models.TextField(blank=True, default='short description2')
     long_description = models.TextField(blank=True, default='long description')
     project_image = models.ImageField(
         upload_to='project_description_images', blank=True)
